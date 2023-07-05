a6fef592a6e43971a60f11bcde5d810b9f2c8149
Rafael Oliveira <rafaelaparecido.oliveirasilva@gmail.com>
2023-07-05 18:55:56 -0300

    feat: Add route component + first route

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,29 @@
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// import SplashScreen from './components/SplashScreen'; 
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        // path: '/',
+        // element: <SplashScreen />,
+      }
+    ]
+  }
+]
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
