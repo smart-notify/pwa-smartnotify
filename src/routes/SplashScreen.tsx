febe6d322d2f2cea90700153e3258cc5f2e3ed49
Rafael Oliveira <rafaelaparecido.oliveirasilva@gmail.com>
2023-07-05 23:22:00 -0300

    feat: Styling SplashScreen Logo

--- src/routes/SplashScreen.tsx
+++ src/routes/SplashScreen.tsx
@@ -6,7 +6,7 @@ import classes from '../css-modules/SplashScreen.module.css';
 function SplashScreen() {
   return (
     <div className={classes.splash}>
-        <img className={classes.splash} src={Logo} width={140} height={100} alt="Logo" />
+        <img src={Logo} width={140} height={100} alt="Logo" />
     </div>
   );
 }
