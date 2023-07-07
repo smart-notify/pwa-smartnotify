eff3952e65a82a50d7792074ec9f5624548fd9dd
Rafael Oliveira <rafaelaparecido.oliveirasilva@gmail.com>
2023-07-06 21:34:48 -0300

    feat: Development form submit + corrections

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 import classes from "../css-modules/Login.module.css";
 import global from "../css-modules/global.module.css";
 import { Link } from "react-router-dom";
@@ -5,6 +7,22 @@ import voltar from "../assets/icones/icone-voltar.svg";
 import logo from "../assets/icones/logo.svg";
 
 function Login() {
+  const [condominio, setCondominio] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const handleCondominioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCondominio(event.target.value);
+  };
+
+  const handleSenhaChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSenha(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(condominio, senha);
+  };
+
   return (
     <div className={classes.loginBackground}>
       <Link to="/acesso" className={classes.backButton}>
@@ -12,10 +30,29 @@ function Login() {
       </Link>
       <div className={classes.loginContent}>
         <img src={logo} width={140} height={100} alt="Logo" />
-        <form action="" autoComplete="off" className={classes.loginForm}>
-          <input type="text" placeholder="Condomínio" required/>
-          <input type="text" placeholder="Senha" required/>
-          <input type="submit" value="Entrar" className={global.loginButton} />
+        <form 
+        onSubmit={handleSubmit}
+        action="" 
+        autoComplete="off" 
+        className={classes.loginForm}>
+          <input
+          onChange={handleCondominioChange}
+          value={condominio}
+          type="text" 
+          placeholder="Condomínio" 
+          required/>
+
+          <input 
+          onChange={handleSenhaChange}
+          value={senha}
+          type="password" 
+          placeholder="Senha" 
+          required/>
+
+          <input 
+          type="submit" 
+          value="Entrar" 
+          className={global.loginButton} />
         </form>
       </div>
     </div>
