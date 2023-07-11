b892865b84cfe612e7542fdeb67f032c8ce52ce8
Rafael Oliveira <rafaelaparecido.oliveirasilva@gmail.com>
2023-07-11 20:10:58 -0300

    feat: Add react-webcam and tesseract

--- src/routes/NotifyScreen.tsx
+++ src/routes/NotifyScreen.tsx
@@ -1,11 +1,53 @@
-import MainMenu from "../components/MainMenu"
+import React, { useState } from "react";
+import Webcam from "react-webcam";
+
+
+const FACING_MODE_ENVIRONMENT = "environment";
 
 function NotifyScreen() {
+  const webcamRef = React.useRef<Webcam>(null);
+  const [image, setImage] = useState("");
+
+  const [facingMode, setFacingMode] = React.useState(FACING_MODE_ENVIRONMENT);
+
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current?.getScreenshot();
+    if (imageSrc){setImage(imageSrc);}
+  }, [webcamRef]);
+
+  let videoConstraints: MediaTrackConstraints = {
+    facingMode: facingMode,
+    width: 470,
+    height: 320
+  };
+
+  console.log(facingMode + videoConstraints);
+
   return (
-    <div>
-      <MainMenu />
-    </div>
-  )
+    <>
+      <div className="webcam-container">
+        <div className="webcam-img">
+          {image === "" ? (
+            <Webcam
+              className="webcam"
+              audio={false}
+              ref={webcamRef}
+              screenshotFormat="image/jpeg"
+              videoConstraints={videoConstraints}
+              screenshotQuality={1}
+            />
+          ) : (
+            <img
+              src={image}
+              alt="Scan"
+              style={{ width: "auto", height: "auto" }}
+            />
+          )}
+        </div>
+        <button >Tirar foto</button>
+      </div>
+    </>
+  );
 }
 
-export default NotifyScreen
+export default NotifyScreen;
