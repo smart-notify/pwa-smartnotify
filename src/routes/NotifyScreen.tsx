6dc4bbaf9f6df132316b5d957c2802db38a9ec5f
Rafael Oliveira <rafaelaparecido.oliveirasilva@gmail.com>
2023-07-11 22:37:29 -0300

    feat: Take a pic and recognize

--- src/routes/NotifyScreen.tsx
+++ src/routes/NotifyScreen.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { createWorker } from "tesseract.js";
+import Tesseract from 'tesseract.js';
 import Webcam from "react-webcam";
 
 import classes from "../css-modules/Notify.module.css";
@@ -8,16 +10,32 @@ import tirarFoto from "../assets/icones/icone-foto.svg";
 const FACING_MODE_ENVIRONMENT = "environment";
 
 function NotifyScreen() {
+  const worker = createWorker({
+    logger: (m) => console.log(m),
+  });
+
   const webcamRef = React.useRef<Webcam>(null);
   const [image, setImage] = useState("");
+  const [result, setResult] = useState("");
 
   const [facingMode] = React.useState(FACING_MODE_ENVIRONMENT);
 
-  const capture = React.useCallback(() => {
+  const capture = React.useCallback(async () => {
     const imageSrc = webcamRef.current?.getScreenshot();
+    const img = new Image();
+    
     if (imageSrc) {
       setImage(imageSrc);
+      img.src = imageSrc;
+
+      const result = await Tesseract.recognize(imageSrc);
+      console.log(result.data.text);
+
+      // guardar resultado em uma variável
+      const resultado = result.data.text;
+      setResult(resultado);
     }
+  
   }, [webcamRef]);
 
   let videoConstraints: MediaTrackConstraints = {
@@ -54,7 +72,9 @@ function NotifyScreen() {
         </div>
         <button className={classes.webcamButton}>
           <img src={tirarFoto} alt="Tirar foto" onClick={capture} />
+          <span className={classes.result}>{result}</span>
         </button>
+
       </div>
     </>
   );
